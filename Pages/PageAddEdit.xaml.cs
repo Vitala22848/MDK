7f1ebcc34d57c46a2e8c5611371248c876329223
Vitala22848 <63116116+Vitala22848@users.noreply.github.com>
2023-10-13 19:15:16 +0300

    Add files via upload

---
+++ Pages/PageAddEdit.xaml.cs
@@ -0,0 +1,76 @@
+﻿using AppLibrary.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppLibrary.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAddEdit.xaml
+    /// </summary>
+    public partial class PageAddEdit : Page
+    {
+        Books _currentBook  = new Books(); 
+        public PageAddEdit(Books booklocal)
+        {
+            InitializeComponent();
+            CmbPublic.ItemsSource = LibraryEntities.GetContext().Publishing.ToList();
+            CmbPublic.SelectedValuePath = "id_publishing";
+            CmbPublic.DisplayMemberPath = "NamePub";
+
+            CmbReader.ItemsSource = LibraryEntities.GetContext().Reader.ToList();
+            CmbReader.SelectedValuePath = "id_Reader";
+            CmbReader.DisplayMemberPath = "Name";
+            
+
+            CmbGenre.ItemsSource = LibraryEntities.GetContext().Genre.ToList();
+            CmbGenre.SelectedValuePath = "id_Genre";
+            CmbGenre.DisplayMemberPath = "NameGe";
+
+            CmbCategory.ItemsSource = LibraryEntities.GetContext().Categories.ToList();
+            CmbCategory.SelectedValuePath = "id_Categories";
+            CmbCategory.DisplayMemberPath = "NameCat";
+
+            CmbFormat.ItemsSource = LibraryEntities.GetContext().Format.ToList();
+            CmbFormat.SelectedValuePath = "id_Format";
+            CmbFormat.DisplayMemberPath = "NameFor";
+
+            if (booklocal != null)
+                _currentBook = booklocal;
+            //создаем контекст
+            DataContext = _currentBook;
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (_currentBook.id_book == 0)
+
+                LibraryEntities.GetContext().Books.Add(_currentBook);
+            
+            LibraryEntities.GetContext().SaveChanges();
+
+            MessageBoxResult boxResult = MessageBox.Show("Данные добавлены. Добавить еще?",
+                "Сообщение", MessageBoxButton.YesNo);
+            if (boxResult == MessageBoxResult.Yes)
+            {
+                TxtNameBook.Clear();
+                TxtCountBook.Clear();
+                //TxtYearBook.Clear();
+                TxtNameBook.Focus();
+            }
+            else
+                Classes.ClassFrame.frmObj.Navigate(new Pages.PageBooks());
+        }
+    }
+}
---
+++ Pages/PageBooks.xaml.cs
@@ -0,0 +1,116 @@
+﻿using AppLibrary.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppLibrary.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageBooks.xaml
+    /// </summary>
+    public partial class PageBooks : Page
+    {
+        public PageBooks()
+        {
+            InitializeComponent();
+         
+            dtgBooks.ItemsSource = LibraryEntities.GetContext().Books.ToList();
+
+            CmbYear.ItemsSource = LibraryEntities.GetContext().Books.Select(x => x.Year).Distinct().ToList();
+            
+            CmbGenre.ItemsSource = LibraryEntities.GetContext().Genre.ToList();
+            CmbGenre.SelectedValue = "id_Genre";
+            CmbGenre.DisplayMemberPath = "NameGe";
+
+        }
+
+        private void CmbYear_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //фильтр книг по году издания
+            int year = (int)(CmbYear.SelectedValue);
+            dtgBooks.ItemsSource = LibraryEntities.GetContext().Books.Where(x=>x.Year == year).ToList();
+
+        }
+
+        private void BtnResetFiltr_Click(object sender, RoutedEventArgs e)
+        {
+            //отмена всех фильтров
+
+            dtgBooks.ItemsSource = LibraryEntities.GetContext().Books.ToList();
+
+        }
+
+        private void CmbGenre_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //фильтр книг по жанру
+            int genre = CmbGenre.SelectedIndex + 1;
+            dtgBooks.ItemsSource = LibraryEntities.GetContext().Books.
+                Where(x => x.id_Genre == genre).ToList();
+
+        }
+
+        private void TxtSearchName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // поиск по названию книги
+            string search = TxtSearchName.Text;
+            dtgBooks.ItemsSource = LibraryEntities.GetContext().Books.
+                Where(x=>x.Name.Contains(search)).ToList();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            //добавление 
+            Classes.ClassFrame.frmObj.Navigate(new Pages.PageAddEdit(null));
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            //редактирование
+            Classes.ClassFrame.frmObj.Navigate(
+                new Pages.PageAddEdit((sender as Button).DataContext as Books ));
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            //удаление
+            var lstForDelete = dtgBooks.SelectedItems.Cast<Books>().ToList();
+            if (MessageBox.Show($"Удалить {lstForDelete.Count()} записей?",
+                "Внимание", MessageBoxButton.YesNo, 
+                MessageBoxImage.Question) == MessageBoxResult.Yes)
+
+                try
+                {
+                    LibraryEntities.GetContext().Books.RemoveRange(lstForDelete);
+                    LibraryEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены");
+                    dtgBooks.ItemsSource = LibraryEntities.GetContext().Books.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+        }
+
+        private void dtgBooks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnListview_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.ClassFrame.frmObj.Navigate(
+                new Pages.PageListView());
+        }
+    }
+}
